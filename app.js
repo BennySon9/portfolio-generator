2c6c5b71955062366052f200439fb2376afaad67
Brandon Benson <brandonbenson09@gmail.com>
2022-05-27 20:26:07 -0400

    added feature to require answers on some questions

--- app.js
+++ app.js
@@ -1,23 +1,53 @@
-const fs = require("fs");
 const inquirer = require("inquirer");
-const generatePage = require("./src/page-template");
+// const fs = require("fs");
+// const generatePage = require("./src/page-template");
 
 const promptUser = () => {
   return inquirer.prompt([
     {
       type: "input",
       name: "name",
-      message: "What is your name?",
+      message: "What is your name? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      },
     },
     {
       type: "input",
       name: "github",
-      message: "Enter your GitHub Username",
+      message: "Enter your GitHub Username (Required)",
+      validate: (githubName) => {
+        if (githubName) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub username!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "confirm",
+      name: "confirmAbout",
+      message:
+        'Would you like to enter some information about yourself for an "About" section?',
+      default: true,
     },
     {
       type: "input",
       name: "about",
       message: "Provide some information about yourself:",
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      },
     },
   ]);
 };
